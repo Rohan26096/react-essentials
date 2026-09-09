c72c72449e6ac6e532a41139739d74bfed58f0fe
Rohan <rohan.muni.24@gmail.com>
2026-09-09 22:04:52 +0530

    UseState in jsx

---
+++ state-demo/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Counter } from './Counter'
+
+function App() {
+
+  return (
+    <div>
+      <h2>Codevolution react course</h2>
+      <Counter/>
+    </div>
+  )
+}
+
+export default App
---
+++ state-demo/src/Counter.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react"
+
+export const Counter = () => {
+    const [count,setCount] = useState(0)
+
+    const handleClick = () => {
+        setCount(count+1)
+    }
+    return <button onClick={handleClick}>Counter: {count} </button>
+}
